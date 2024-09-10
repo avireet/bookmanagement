c34daefcdd42af406cc0cf6fd8f89a364f4c184d
avireet <avireetkaur@yahoo.comgit config --global user.email avireetkaur@yahoo.com>
2024-09-10 23:17:31 +0530

    code

---
+++ middleware/auth.js
@@ -0,0 +1,14 @@
+const auth = require('basic-auth');
+const credentials = { name: 'admin', pass: 'password' };  
+const basicAuth = (req, res, next) => {
+    const user = auth(req);
+
+    if (user && user.name === credentials.name && user.pass === credentials.pass) {
+        return next();
+    } else {
+        res.setHeader('WWW-Authenticate', 'Basic realm="example"');
+        res.status(401).json({ message: 'Authentication required' });
+    }
+};
+
+module.exports = basicAuth;
